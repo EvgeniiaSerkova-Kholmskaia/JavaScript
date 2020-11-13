7f8d6bc38133f673ce868d80ef3eaad031e4a92f
Evgeniia Serkova <dgein-s@mail.ru>
2020-11-13 16:02:06 +0300

    2 commit

--- js/homework.js
+++ js/homework.js
@@ -14,23 +14,26 @@ height = height * 2;
 console.log(height);
 
 // задача 2.
-let n = 37;
-let sum = 0;
+let n = 67;
 let number = n % 10;
-let next = n - number;
-let division = next / 10;
-sum = division + number;
-
+let division = (n - number) / 10;
+let sum = division + number;
 let info = `Значение n равно ${n}, сумма его цифр равна ${sum}`
 console.log(info);
 
 //  задача 3.
-let a = 7;
-let b = 6;
 
-a *= 2;
-a -= 8;
-b **= 2;
-b -= 1;
-b /= 5;
+let a = 31
+let b = 9;
+
+// a *= 2;
+// a -= 8;
+// b **= 2;
+// b -= 1;
+// b /= 5;
+// console.log(a, b);
+
+a = b + a;
+b = a - b;
+a = a - b;
 console.log(a, b);
